b1b427af850f0ce1641a23d284f6a27e3edfad9e
Karthik Mallavarapu <karthik.mallavarapu@gmail.com>
2014-11-13 14:58:32 +0200

    converts tabs to spaces

--- crawler.rb
+++ crawler.rb
@@ -4,50 +4,50 @@ require 'open-uri'
 
 module Crawler
 
-	EXCLUDE_PATTERN = /.(jpg|jpeg|png|gif|pdf|svg|mp4)\z/
+  EXCLUDE_PATTERN = /.(jpg|jpeg|png|gif|pdf|svg|mp4)\z/
 
-	def scrape_page(url)
-		page = get_page(url)
-		return [] if page.nil?
-		links = page.css('a')
-		urls = links.map { |link|	link['href'] }
-		urls.compact!
-		urls.uniq!
-		sanitize_urls(urls).compact.uniq
-	end
+  def scrape_page(url)
+    page = get_page(url)
+    return [] if page.nil?
+    links = page.css('a')
+    urls = links.map { |link|  link['href'] }
+    urls.compact!
+    urls.uniq!
+    sanitize_urls(urls).compact.uniq
+  end
 
-	def get_page(url)
-		begin
-			page = Nokogiri::HTML(open(url))
-		rescue Exception => e
-			puts "Warning: Get page for #{url} resulted in #{e.message}"
-		end
-	end
+  def get_page(url)
+    begin
+      page = Nokogiri::HTML(open(url))
+    rescue Exception => e
+      puts "Warning: Get page for #{url} resulted in #{e.message}"
+    end
+  end
 
-	def sanitize_urls(urls)
-		urls.map do |link|
-			begin
-				uri = URI(URI.encode(link))
-				if uri.absolute? && uri.hostname == domain &&
-				(uri.scheme == "http" || uri.scheme == "https")
-					URI.decode(uri.to_s)
-				elsif uri.relative?
-					URI.decode(URI(URI.encode(base_url)).merge(uri).to_s)
-				end
-			rescue Exception => e
-				puts "Warning: #{e.message}"
-				next
-			end
-		end
-	end
+  def sanitize_urls(urls)
+    urls.map do |link|
+      begin
+        uri = URI(URI.encode(link))
+        if uri.absolute? && uri.hostname == domain &&
+        (uri.scheme == "http" || uri.scheme == "https")
+          URI.decode(uri.to_s)
+        elsif uri.relative?
+          URI.decode(URI(URI.encode(base_url)).merge(uri).to_s)
+        end
+      rescue Exception => e
+        puts "Warning: #{e.message}"
+        next
+      end
+    end
+  end
 
   def disallowed?(url)
     !!(url =~ EXCLUDE_PATTERN)
   end
 
-	def reachable?(url)
-		return true if get_page(url)
-		false
-	end
+  def reachable?(url)
+    return true if get_page(url)
+    false
+  end
 
 end
--- site.rb
+++ site.rb
@@ -1,5 +1,5 @@
 require_relative 'crawler'
-
+ 
 class Site
 
   include Crawler
